81aa254029b9b0c0b2624fb28d86afcf492ac5c2
AlanOmes <82296929+AlanOmes@users.noreply.github.com>
2022-01-24 23:33:11 -0300

    Agrega ejercicios

--- Python2.py
+++ Python2.py
@@ -63,6 +63,8 @@ while EmpezarNuevo == ("s") or EmpezarNuevo == ("si"):
 
 # Escribe un programa que te permita jugar a una version simplificada del juego Master Mind. El juego consistira en adivinar una cadena de numeros distintos. Al principio, el programa debe pedir la longitud de la cadena (de 2 a 9 cifras). Despues el programa debe ir pidiendo que intentes adivinar la cadena de numeros. En cada intento, el programa informara de cuantos numeros han sido acertados (el programa considerara que se ha acertado un numero si coincide el valor y la posicion).
 
+'''
+
 import random
 
 
@@ -77,7 +79,7 @@ for i in range(longitud):
     i = random.randint(1, 9)
     numero.append(i)
 
-print (numero)
+#print (numero)
 
 aciertos = 0
 
@@ -100,7 +102,29 @@ while aciertos != longitud:
     else:
         print (f'\nCon {intento} has adivinado {aciertos} valores.')
 
+'''
 
-    
+# Escribe un programa que pida dos palabras y diga si riman o no. Si coinciden las tres ultimas letras tiene que decir que riman. Si coinciden solo las dos ultimas tiene que decir que riman un poco y si no, que no riman. 
 
+'''
+
+def palabras_que_riman(palabra1, palabra2):
+    rima = ''
+    if palabra1[-3] + palabra1[-2] + palabra1[-1] == palabra2[-3] + palabra2[-2] + palabra2[-1]:
+        rima = 'Las palabras riman'
+    elif palabra1[-2] + palabra1[-1] == palabra2[-2] + palabra2[-1]:
+        rima = 'Las palabras riman un poco'
+    else:
+        rima = 'Las palabras no riman'
+    return rima
+
+p = input('Ingrese la primera palabra: ')
+p2 = input('Ingrese la segunda palabra: ')
+
+print (f'\r\n{palabras_que_riman(p, p2)}')
+
+
+'''
+
+# Has un programa que pida al usuario una cantidad de dolares, una tasa de interes y un numero de anios. Muestra por pantalla en cuanto se habra convertido el capital inicial transcurridos esos anios si cada anio se aplica la tasa de interes introducida. Recordar que un capital C dolares a un interes del x por cien durante n anios se convierte en C * (1 + x/100)elevado a n (anios). Probar el programa sabiendo que una cantidad de 10000 dolares al 4.5% de interes anual se convierte en 24117.14 dolares al cabo de 20 anios.
 
